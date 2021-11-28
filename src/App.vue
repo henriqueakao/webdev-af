a2a2ce63f1f8a46a02ec4766f75f58a0824b6a8a
Henrique Akao <henrique.tanioka@itixti.com.br>
2021-11-28 19:59:53 -0300

    appbar + navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+  <AppToolbar></AppToolbar>
+  
+  <NavigationDrawer></NavigationDrawer>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
+
+<script>
+  import NavigationDrawer from '@/components/NavigationDrawer'
+  import AppToolbar from '@/components/AppToolbar'
+
+  export default {
+    name: 'App',
+    components: {
+      NavigationDrawer,
+      AppToolbar
+    }
+  }
+</script>
---
+++ src/components/NavigationDrawer.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-navigation-drawer permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Desenvolvimento Web
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Avaliação Final
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+</template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        items: [
+          { title: 'Albums', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'Share', icon: 'mdi-antenna' },
+        ],
+        right: null,
+      }
+    },
+  }
+</script>
